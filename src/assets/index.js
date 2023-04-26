d796b5e7e02912ed56645cb3d8c2d4fb4a6a9c0a
anshulghogre4 <anshulghogre4@gmail.com>
2023-04-26 16:04:44 +0530

    completed mid part

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -6,10 +6,9 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import java from "./tech/java.png";
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
+import csharp from "./tech/csharp.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -19,16 +18,17 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-
+import mysql from "./tech/mysql.png";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-
+import dotnet from "./tech/dotnet.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import bootstrap from "./tech/bootstrap.png";
+import springboot from "./tech/springboot.png";
 
 export {
   logo,
@@ -40,8 +40,6 @@ export {
   menu,
   close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
@@ -51,7 +49,6 @@ export {
   redux,
   tailwind,
   typescript,
-  threejs,
   meta,
   shopify,
   starbucks,
@@ -59,4 +56,10 @@ export {
   carrent,
   jobit,
   tripguide,
+  java,
+  csharp,
+  mysql,
+  dotnet,
+  springboot,
+  bootstrap,
 };
